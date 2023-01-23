3254daa2a75c870c097db8b32a3c589b7743d4b1
Victor Schmoegel <victorschmoegel@hotmail.com>
2023-01-23 16:36:11 -0300

    momento assíncrono

---
+++ fundamentos/secao3-intro-javascript/dia1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,122 @@
+/*let age = 20;
+age = 30;
+let name = "hulk";
+
+console.log(age);
+console.log(name);*/
+
+/*const myName = "Victor";
+const birthCity = "Brasília";
+const birthYear = 1996;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+ 
+birthCity = "Florianópolis";
+
+console.log(birthCity);*/
+
+/*let movie = "Avengers"; //string literal
+let score = 10.5; //number literal
+let isValid = true; //boolean
+let name; //undefined --> Pq ainda não recebeu nenhum valor
+let color = null; //redefinição de valor*/
+
+/*let salary =3500;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(3 ** 3);*/
+
+/*let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);*/
+
+
+/*const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log(base);
+console.log(heigth);
+console.log(area);
+
+const perimeter = base + base + heigth + heigth;
+
+console.log(perimeter);*/
+
+
+
+
+/*let trybe = 14;
+
+if (trybe >= 14 && trybe < 14.40) {
+    console.log("esquenta");
+}
+
+else if (trybe >= 16.30 && trybe < 17.50) {
+    console.log("aula ao vivo");
+}
+
+else if (trybe >= 19.40 && trybe < 20.00) {
+    console.log("fechamento");
+}
+
+else {
+    console.log("Fora dos momento síncronos");
+}
+
+const nota = 66;
+
+ if (nota >= 80) {
+    console.log("Parabéns, você faz parte do grupo de pessoas aprovadas");
+ }
+
+ else if (nota < 80 && nota >= 60) {
+    console.log("Você está na lista de espera");
+ }
+
+ else if (nota < 60) {
+    console.log("Infelizmente você reprovou");
+ }*/
+
+ const currentHour = 20;
+ let message = "";
+
+ if (currentHour >= 22) {
+    message = "não deveríamos comer nada";
+ }
+
+ else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da NodeIterator, vamos jantar";
+ }
+
+ else if (currentHour >= 14 && currentHour < 18) {
+    message = "vamos fazer bolo";
+ }
+
+ else if (currentHour >= 11 && currentHour < 14) {
+    message = "Almoço";
+ } 
+
+ else if (currentHour >= 4 && currentHour < 11) {
+    message = "café passado";
+ }
+
+ console.log(message);
+ 
+ 
+
