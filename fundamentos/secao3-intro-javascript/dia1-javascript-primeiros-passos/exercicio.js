36c3b89e0d6d881b93a33eaeca6894259529df74
Victor Schmoegel <victorschmoegel@hotmail.com>
2023-01-24 13:02:00 -0300

    terminando exercicios

--- fundamentos/secao3-intro-javascript/dia1-javascript-primeiros-passos/exercicio.js
+++ fundamentos/secao3-intro-javascript/dia1-javascript-primeiros-passos/exercicio.js
@@ -51,3 +51,64 @@ else {
     console.log(false);
 }
 
+const percentage = 90;
+
+if (percentage < 0 || percentage > 100) {
+    console.error("porcentagem inválida")
+}
+
+else if (percentage >= 90) {
+    console.log("A")
+}
+else if (percentage >=80) {
+    console.log("B")
+}
+else if (percentage >=70) {
+    console.log("C")
+}
+else if (percentage >=60) {
+    console.log("D")
+}
+else if (percentage >=50) {
+    console.log("E")
+}
+else {
+    console.log("F")
+}
+
+const numero1 = 31;
+const numero2 = 23;
+const numero3 = 11;
+
+if (numero1 % 2 == 0 || numero2 % 2 == 0 || numero3 % 2 == 0) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+
+const custo = 14;
+const venda = 56;
+const imposto = 0.20;
+
+if (custo < 0 || venda < 0) {
+    console.error("valor de entrada menor que zero");
+    return;
+}
+
+const valorCustoTotal = custo + imposto;
+const lucro = venda - valorCustoTotal;
+const lucroTotal = lucro * 1000;
+
+console.log(lucroTotal);
+
+
+
+const bruto = 5000;
+const aliquota = 0.11;
+const base = 4450;
+const ir = 0.225;
+
+let liquido = base - 365.12;
+
+console.log(liquido);
