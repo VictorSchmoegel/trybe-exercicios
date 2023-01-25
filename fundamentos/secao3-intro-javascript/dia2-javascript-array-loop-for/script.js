689f4a7de6b04b7b8a555896991fd4d0134377c6
Victor Schmoegel <victorschmoegel@hotmail.com>
2023-01-25 14:26:02 -0300

    terminando exercícios do dia

--- fundamentos/secao3-intro-javascript/dia2-javascript-array-loop-for/script.js
+++ fundamentos/secao3-intro-javascript/dia2-javascript-array-loop-for/script.js
@@ -66,7 +66,101 @@ for (let index = 0; index < groceryList.length; index += 1) {
 }*/
 
 
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+/*let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for (let index of names) {
     console.log(index);
+}*/
+
+/* ======== Exercício 1 - Lidando com Arrays ======== */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+let mediaArit = 0;
+let maiorNum = 0;
+let valorImpar = 0;
+let menorNum = numbers[0];
+let array25 = [];
+let valorPor2 = 0;
+
+
+// Percorrendo Array
+for (let index = 0; index < numbers.length; index += 1)
+console.log(numbers[index]);
+
+console.log("");
+
+// Somando valores do Array
+for (let index = 0; index <numbers.length; index += 1) {
+    result += numbers[index];
+}
+console.log(result);
+
+console.log("");
+
+// Média aritimética do Array
+for (let index = 0; index < numbers.length; index += 1) {
+    resultMediaArit = (result / numbers.length);
+}
+
+console.log(resultMediaArit);
+
+console.log("");
+
+// Imprimindo mensagem do valor
+
+if (resultMediaArit > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
+
+console.log("");
+
+// Descobrindo qual o maior valor do Array
+
+for (let index = 0; index < numbers.length; index += 1){
+    if (numbers[index] > maiorNum) {
+        maiorNum = numbers[index];
+    }
+}
+console.log(maiorNum);
+
+console.log("");
+
+// Descobrindo quantidade de valores ímpar
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        valorImpar += 1;
+    }
+}
+if (valorImpar === 0) {
+    console.log("nenhum valor ímpar encontrado");
+} else {
+    console.log(valorImpar);
+}
+
+console.log("");
+
+// Descobrindo o menor valor
+
+for (let index = 1; index < numbers.length; index += 1){
+    if (numbers[index] < menorNum) {
+        menorNum = numbers[index];
+    }
+}
+console.log(menorNum);
+
+console.log("");
+
+// Criando uma array com for e imprimindo o resultado por 2
+
+for (let index = 0; index <= 25; index += 1){
+    array25.push(index);
+}
+
+console.log(array25);
+
+for (let index = 0; index < array25; index += 1){
+    console.log(array25[index] / 2);
 }
