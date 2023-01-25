8c3e152f64e45efcb6cfa48dd4cd6ac6d5047483
Victor Schmoegel <victorschmoegel@hotmail.com>
2023-01-25 10:01:38 -0300

    comenaçando exercícios

---
+++ fundamentos/secao3-intro-javascript/dia2-javascript-array-loop-for/script.js
@@ -0,0 +1,72 @@
+/*let pizzas = ["4 Queijos", "Frango com Catupiry", "marguerita", "Palmito", "chocolate"];
+
+pizzas.push("Peperoni");
+
+
+for (let index = 0; index < pizzas.length; index += 1) {
+    console.log(pizzas[index]);
+}*/
+
+/*let tasksList = ["tomar cafe", "reunião", "brincar com o cachorro"];*/
+
+//console.log(tasksList.length);
+
+/*let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask); PRIMEIRO VALOR DA LISTA */
+
+/*let searchForFirstTask = tasksList[tasksList.length - 1];
+console.log(searchForFirstTask); ULTIMO VALOR DA LISTA */
+
+/*tasksList.push("fazer exercicios Trybe");
+console.log(tasksList); ACRESCENTA ALGO NO FINAL DA LISTA */
+
+/*tasksList.unshift("comer pizza");
+console.log(tasksList); ACRESCENTE ALGO NO INICIO DA LISTA */
+
+/*tasksList.pop();
+console.log(tasksList); EXCLUI O ULTIMO DA LISTA */
+
+/*tasksList.shift();
+console.log(tasksList); EXCLUI O PRIMEIRO DA LISTA */
+
+/*let indexOfTask = tasksList.indexOf("reunião");
+console.log(indexOfTask); RETORNA O VALOR DO ITEM DA LISTA */
+
+/*let menu = ["home", "serviços", "portifolio", "links"];
+let menuServices = menu[1];
+
+console.log(menuServices);
+ 
+let indexOfPortifolio = menu.indexOf("portifolio");
+
+console.log(indexOfPortifolio);
+
+menu.push("contato");
+
+console.log(menu);*/
+
+
+/*let numero = 5;
+
+for (let contador = 1; contador <= 9; contador++) {
+    console.log("");
+    console.log(numero * contador);
+}
+
+let listaDeNomes = ["Joana", "Maria", "Lucas"];
+for (let indice =0; indice < listaDeNomes.length; indice += 1) {
+    let mensagem = "Boas vindas, " + listaDeNomes[indice] + "!";
+    console.log(mensagem)
+}*/
+
+/*let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+for (let index = 0; index < groceryList.length; index += 1) {
+    const element = groceryList[index];
+    console.log(element);
+}*/
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let index of names) {
+    console.log(index);
+}
