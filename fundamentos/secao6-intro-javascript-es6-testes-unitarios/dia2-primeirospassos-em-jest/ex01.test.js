6d94dc891c820cbe7aa76527a327806d4230cf2a
Victor Schmoegel <victorschmoegel@hotmail.com>
2023-03-01 17:26:28 -0300

    ex

---
+++ fundamentos/secao6-intro-javascript-es6-testes-unitarios/dia2-primeirospassos-em-jest/ex01.test.js
@@ -0,0 +1,9 @@
+const myRemove = require('./ex01');
+
+describe('a função myRemove(arr, item)', () => {
+    it('requisito 01', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
+});
