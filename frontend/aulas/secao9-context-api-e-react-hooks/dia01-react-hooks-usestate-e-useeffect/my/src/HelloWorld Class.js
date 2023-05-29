50e51c3774fe85e158f612b25d700456abbce138
Victor Schmoegel <victorschmoegel@gmail.com>
2023-05-29 15:08:28 -0300

    aula 1

---
+++ frontend/aulas/secao9-context-api-e-react-hooks/dia01-react-hooks-usestate-e-useeffect/my/src/HelloWorld Class.js
@@ -0,0 +1,11 @@
+import React from  'react';
+
+class HelloWorldClass extends React.Component {
+  render() {
+    return (
+      <h1>Olá Mundo - Copmponente de Classe</h1>
+    );
+  }
+}
+
+export default HelloWorldClass;
